7b4f606d83bb9c3aab702ddd03312dd29e41fb0e
cuong-van <zerocuong12345@gmail.com>
2022-02-28 23:55:45 +0700

    add file

---
+++ src/main/java/vns/nvc/db/selectcompare/DBUtil.java
@@ -0,0 +1,78 @@
+package vns.nvc.db.selectcompare;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class DBUtil {
+
+    static class DriverShim implements Driver {
+        private final Driver driver;
+        DriverShim(Driver d) {
+            this.driver = d;
+        }
+        public boolean acceptsURL(String u) throws SQLException {
+            return this.driver.acceptsURL(u);
+        }
+        public Connection connect(String u, Properties p) throws SQLException {
+            return this.driver.connect(u, p);
+        }
+        public int getMajorVersion() {
+            return this.driver.getMajorVersion();
+        }
+        public int getMinorVersion() {
+            return this.driver.getMinorVersion();
+        }
+        public DriverPropertyInfo[] getPropertyInfo(String u, Properties p) throws SQLException {
+            return this.driver.getPropertyInfo(u, p);
+        }
+        public boolean jdbcCompliant() {
+            return this.driver.jdbcCompliant();
+        }
+
+        @Override
+        public Logger getParentLogger() {
+            return null;
+        }
+    }
+
+    private static final String DB_DRIVER_PATH="driver.file.path";
+    private static final String DB_DRIVER_CLASS="driver.class.name";
+    private static final String DB_USERNAME="db.username";
+    private static final String DB_PASSWORD="db.password";
+    private static final String DB_URL ="db.url";
+
+    private static Connection connection = null;
+    private static Properties properties = null;
+
+    static {
+        try {
+            properties = new Properties();
+            properties.load(new FileInputStream(Paths.get("").toAbsolutePath() + "/output/db.properties"));
+
+            URL u = new URL(String.format("jar:file:%s!/", properties.getProperty(DB_DRIVER_PATH)));
+            String classname = properties.getProperty(DB_DRIVER_CLASS);
+            URLClassLoader ucl = new URLClassLoader(new URL[] { u });
+            Driver d = (Driver)Class.forName(classname, true, ucl).getDeclaredConstructor().newInstance();
+            DriverManager.registerDriver(new DriverShim(d));
+        } catch (ClassNotFoundException | IOException | SQLException | InstantiationException
+                | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        try {
+            connection = DriverManager.getConnection(properties.getProperty(DB_URL),properties.getProperty(DB_USERNAME) , properties.getProperty(DB_PASSWORD) );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
---
+++ src/main/java/vns/nvc/db/selectcompare/Main.java
@@ -0,0 +1,101 @@
+package vns.nvc.db.selectcompare;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection connection = DBUtil.getConnection();
+        assert connection != null;
+
+        Datatable dataSelect = new Datatable();
+        System.out.println(new Date());
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM students");
+//            CSVWriter csvWriter = new CSVWriter(new FileWriter("D:/WORKING/NVC_PJ/db_select_compare/db_log.csv"));
+//            csvWriter.writeAll(rs, true);
+            dataSelect.loadQueryResult(rs);
+            System.out.println(dataSelect.getRowCount());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Datatable dataLog = new Datatable();
+        System.out.println(new Date());
+        try {
+            CSVReader reader = new CSVReader(new FileReader("D:/WORKING/NVC_PJ/db_select_compare/output/db_log.csv"));
+            dataLog.loadCSVReader(reader);
+            System.out.println(dataLog.getRowCount());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(new Date());
+
+        Datatable[] datatables = filterDatatablesWithStatus(List.of("id"), dataLog, dataSelect);
+
+        System.out.println(datatables[0].getRowCount());
+        System.out.println(datatables[1].getRowCount());
+        System.out.println(datatables[2].getRowCount());
+        System.out.println(new Date());
+    }
+
+    public static Datatable[] filterDatatablesWithStatus(List<String> columnKeys, Datatable logData, Datatable data) {
+        Datatable[] datatables = new Datatable[3];
+        datatables[0] = data; // INSERT
+        datatables[1] = new Datatable(); // UPDATE
+        datatables[2] = new Datatable(); // DELETE
+
+        //datatables[0].addAllColumn(logData.getColumns());
+        datatables[1].addAllColumn(logData.getColumns());
+        datatables[2].addAllColumn(logData.getColumns());
+
+        List<Object> rowDataLog;
+        List<Object> rowData;
+
+        Map<String, List<Object>> dataMapper = new HashMap<>();
+        String key;
+
+        for (int i=0; i<data.getRowCount(); i++) {
+            key = "";
+            for (String columnKey : columnKeys) {
+                 key = data.getCell(columnKey, i) + "%_%";
+            }
+            dataMapper.put(key, data.getRow(i));
+        }
+
+        loopDataLog:
+        for (int i=0; i<logData.getRowCount(); i++) {
+            key = "";
+            for (String columnKey : columnKeys) {
+                key = logData.getCell(columnKey, i) + "%_%";
+            }
+
+            rowDataLog = logData.getRow(i);
+            rowData = dataMapper.get(key);
+
+            if (rowData != null) {
+                // Remove data UPDATED <=> rest data INSERTED
+                data.removeRow(rowData);
+                for (int ii=0; ii<rowDataLog.size(); ii++) {
+                    if (!String.valueOf(rowDataLog.get(ii)).equals(String.valueOf(rowData.get(ii)))) {
+                        // Add data UPDATED
+                        datatables[1].addRow(rowData);
+                        continue loopDataLog;
+                    }
+                }
+            } else {
+                // Add data DELETED
+                datatables[2].addRow(rowDataLog);
+            }
+        }
+
+        return datatables;
+    }
+}
